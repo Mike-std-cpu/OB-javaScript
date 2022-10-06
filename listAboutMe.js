654bacdc3bf65d37f5f121cc0a91a1eb1f35e9ba
Mike Andrade <hoodloom07@outlook.es>
2022-10-06 11:57:22 -0500

    list about me

---
+++ listAboutMe.js
@@ -0,0 +1,15 @@
+const me = {
+    name:"Miguel Andrade",
+    age:"24",
+    development:true,
+    bday: new Date("Dec 30 1997"),
+    fbook:{
+        tittle: "Harry Potter and the prisoner of azkaban",
+        author: "J. K. Rowwling",
+        date: new Date("Jul 08 1999"),
+        url:new URL("https://en.wikipedia.org/wiki/Harry_Potter_and_the_Prisoner_of_Azkaban#:~:text=Harry%20Potter%20and%20the%20Prisoner%20of%20Azkaban%20was%20released%20in,was%20released%201%20October%202001.")
+    }
+
+}
+
+console.log(me);
