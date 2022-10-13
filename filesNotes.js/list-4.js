9db7ec35e7bf228de8def7e1562cdf49f57ffbc7
Mike Andrade <hoodloom07@outlook.es>
2022-10-12 20:27:16 -0500

    methods from and some

--- filesNotes.js/list-4.js
+++ filesNotes.js/list-4.js
@@ -30,3 +30,50 @@ function compararArrays(lista1, lista2){
 }
 
 console.log(compararArrays(list1,list2));
+
+//.some()
+
+const arregloDos = [1, 5, -9, 25, -9, 60, 2, 14, 93, -55, 78, 98, 20];
+
+const resul = arregloDos.some(valor => valor < -80);
+
+console.log(resul);
+
+const objetos = [
+    {name: "Cristhian", age: 26},
+    {name: "Tristan",age: 25},
+    {name: "Mario", age: 24},
+    {name: "Miguel", age: 25},
+    {name: "Gerry", age: 31}
+]
+
+const miguel = objetos.some(persona => persona.name == "Miguel");
+console.log(miguel);
+
+
+// .from:  El método Array.from() crea una nueva instancia de Array a partir de un objeto iterable.
+const str = "Hello my name is Mike";
+const arrayStr = Array.from(str);
+console.log(arrayStr);
+//Consola: 
+// [ 'H',
+//   'e',
+//   'l',
+//   'l',
+//   'o',
+//   ' ',
+//   'm',
+//   'y',
+//   ' ',
+//   'n',
+//   'a',
+//   'm',
+//   'e',
+//   ' ',
+//   'i',
+//   's',
+//   ' ',
+//   'M',
+//   'i',
+//   'k',
+//   'e' ]
