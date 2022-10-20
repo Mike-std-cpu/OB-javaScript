153c37bc7310e968bc0ae42c992c1e8dab016e17
Mike Andrade <hoodloom07@outlook.es>
2022-10-19 20:21:38 -0500

    Functions and type it

---
+++ filesNotes.js/functions.js
@@ -0,0 +1,9 @@
+/**
+ * Las fucniones son bloques de codidigo con funciones q ue nosotros le demos al momento de declararlas.
+ */
+
+function saludar (name){
+    console.log(`Hello ${name}, how are u? :)`);
+}
+
+saludar("Miguel");
