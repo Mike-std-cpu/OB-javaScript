c249c4537ef0f8f93ac8cca7e9a4a0270b140ad6
Mike Andrade <hoodloom07@outlook.es>
2022-10-21 13:49:16 -0500

    changes about .json and index

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log("Hola estoy saliendo por pantalla");
+console.info("Hola, soy un mensaje informativo");
+console.debug("Esto es un mns debug");
+console.warn("Esto es un mensaje de advertencia");
+console.error("Esto es un error");
