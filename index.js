5ff5c06be056609792451a36f76bb169945af20b
Mike Andrade <hoodloom07@outlook.es>
2022-10-21 19:25:55 -0500

    Modules with commonJS export and imports

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const logger = require('./logger')
+const logger = require('./logger');
+const {moduloMates} = require('./modulos/matematicas.js');
+
 //***
 logger.info("Hola, soy un mensaje informativo");
 logger.debug("Esto es un mns debug");
@@ -9,4 +11,16 @@ logger.error("Esto es un error");
  * Instalacion de winstone, libreria de node.
  * https://www.npmjs.com/package/winston
  */
+
+// Seccion de Matematicas
+
+const moduloSuma = moduloMates.suma(5,5);
+const moduloResta = moduloMates.resta( 10, 5);
+const moduloMultipli = moduloMates.multipl(5, 5);
+const moduloDiv = moduloMates.division(10, 10);
+
+console.log(moduloSuma);
+console.log(moduloResta);
+console.log(moduloMultipli);
+console.log(moduloDiv);
 
---
+++ modulos/matematicas.js
@@ -0,0 +1,23 @@
+function suma(a,b){
+    return a + b;
+}
+
+function resta(a,b){
+    return a - b;
+}
+
+function multipl(a,b){
+    return a * b;
+}
+
+function division(a,b){
+    return a / b;
+}
+
+//Exportacion de funciones
+module.exports = {
+    suma,
+    resta,
+    multipl,
+    division
+}
