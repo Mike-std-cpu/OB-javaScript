27ddaeed6881cc721f718e968c19d7ed00fb7932
Mike Andrade <hoodloom07@outlook.es>
2022-10-24 06:27:41 -0500

    Instances and classes

---
+++ filesNotes.js/objetos/person.js
@@ -0,0 +1,23 @@
+class Person{
+    constructor(name, age, isDeveloper){
+        this.name = name;
+        this.age = age;
+        this.isDeveloper = isDeveloper;
+    }
+
+    saludo() {
+        console.log(`Hello 🌚, my name is ${this.name} and I'm ${this.age} years old`);
+    }
+}
+
+// Se instancio la clase person con nuevas objetos
+const mike = new Person("Mike",25,true);
+console.log(mike);
+mike.saludo();
+// Otra instancia
+const sara = new Person("Sarai", 24, false);
+console.log(sara);
+sara.saludo();
+
+//Es como typeOf
+console.log(sara instanceof Person);
