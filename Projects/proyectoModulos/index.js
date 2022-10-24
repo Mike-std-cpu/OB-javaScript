6b4951ac922d71ae4ca2ae012577a540577c044b
Mike Andrade <hoodloom07@outlook.es>
2022-10-24 00:45:44 -0500

    Module Proyect: 4. Controller eported, index created, operations test

---
+++ Projects/proyectoModulos/index.js
@@ -0,0 +1,9 @@
+import {suma, multiplica} from "./controller.js"
+
+const sma1 = suma(1,2);
+const sma2 = suma(4,5);
+console.log(`La suma 1 es de: ${sma1}`);
+console.log(`La suma 2 es de: ${sma2}`);
+
+const multi = multiplica(sma1, sma2);
+console.log(`La multiplicacion de ${sma1} y ${sma2} es de: ${multi}`);
