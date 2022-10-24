a44066017a92674029565b7ac12821cfc892012c
Mike Andrade <hoodloom07@outlook.es>
2022-10-24 00:51:58 -0500

    Modules proyect: 6. Use chalk with clg green color

--- Projects/proyectoModulos/index.js
+++ Projects/proyectoModulos/index.js
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import {suma, multiplica} from "./controller.js"
 
 const sma1 = suma(1,2);
@@ -6,4 +7,4 @@ console.log(`La suma 1 es de: ${sma1}`);
 console.log(`La suma 2 es de: ${sma2}`);
 
 const multi = multiplica(sma1, sma2);
-console.log(`La multiplicacion de ${sma1} y ${sma2} es de: ${multi}`);
+console.log(chalk.green(`El resultado de la multiplicacion de ${sma1} y ${sma2} es de: ${multi}`));
